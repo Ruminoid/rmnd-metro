3492fb6aa62a4916c0178d60e93c4c3b5bd12e6f
Il Harper <afanyiyu@hotmail.com>
2021-04-20 21:01:10 +0800

    feat: Add MessageBox

---
+++ src/MetroControls/Dialogs/MessageBox.axaml.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace Ruminoid.Common2.Metro.MetroControls.Dialogs
+{
+    public class MessageBox : Window
+    {
+        #region Constructor
+
+        public MessageBox()
+        {
+            throw new InvalidOperationException();
+        }
+
+        private MessageBox(
+            string title,
+            string content,
+            bool showNoButton = true)
+        {
+            InitializeComponent();
+
+            this.FindControl<TextBlock>("TitleBlock").Text = title;
+            this.FindControl<TextBlock>("ContentBlock").Text = content;
+
+            if (!showNoButton)
+            {
+                this.FindControl<Button>("YesButton").Content = "çD";
+                this.FindControl<Button>("NoButton").IsVisible = false;
+            }
+
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        #endregion
+
+        public static Task<bool> ShowAndGetResult(
+            string title,
+            string content,
+            Window parent,
+            bool showNoButton = true)
+        {
+            MessageBox window = new(title, content, showNoButton);
+            return window.ShowDialog<bool>(parent);
+        }
+
+        private void YesClick(object sender, RoutedEventArgs e) => Close(true);
+
+        private void NoClick(object sender, RoutedEventArgs e) => Close(false);
+    }
+}
