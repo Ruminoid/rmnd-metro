84d0575a5a3b74efd468104914ded3d543ffb17a
Il Harper <afanyiyu@hotmail.com>
2021-04-22 15:52:11 +0800

    feat: Add More Controls

---
+++ src/MetroControls/DrawingIcon.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+using JetBrains.Annotations;
+
+namespace Ruminoid.Common2.Metro.MetroControls
+{
+    public class DrawingIcon : TemplatedControl
+    {
+        #region Constructor
+
+        static DrawingIcon()
+        {
+            WidthProperty.OverrideDefaultValue<DrawingIcon>(16);
+            HeightProperty.OverrideDefaultValue<DrawingIcon>(16);
+        }
+
+        #endregion
+
+        #region Icon
+
+        [UsedImplicitly]
+        public static readonly StyledProperty<Drawing> IconProperty =
+            AvaloniaProperty.Register<DrawingIcon, Drawing>(nameof(Icon));
+
+        public Drawing Icon
+        {
+            get => GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        #endregion
+    }
+}
---
+++ src/MetroControls/DrawingIconButton.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace Ruminoid.Common2.Metro.MetroControls
+{
+    public class DrawingIconButton : Button
+    {
+        #region Icon
+
+        public static readonly StyledProperty<Drawing> IconProperty =
+            AvaloniaProperty.Register<DrawingIconButton, Drawing>(nameof(Icon));
+
+        public Drawing Icon
+        {
+            get => GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        #endregion
+    }
+}
