a259113aed0001fb750dae7a607ccc55e5a722e7
Il Harper <afanyiyu@hotmail.com>
2021-04-22 15:02:37 +0800

    feat: Add Hyperlink Control

---
+++ src/MetroControls/Hyperlink.cs
@@ -0,0 +1,33 @@
+﻿// https://github.com/AvaloniaUtils/MessageBox.Avalonia/blob/master/src/MessageBox.Avalonia/Controls/Hyperlink.cs
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using JetBrains.Annotations;
+using Ruminoid.Common2.Utils.Net;
+
+namespace Ruminoid.Common2.Metro.MetroControls
+{
+    public class Hyperlink : TextBlock
+    {
+        private string _url;
+
+        [UsedImplicitly]
+        public static readonly DirectProperty<Hyperlink, string> UrlProperty
+            = AvaloniaProperty.RegisterDirect<Hyperlink, string>(nameof(Url), o => o.Url, (o, v) => o.Url = v);
+
+        public string Url
+        {
+            get => _url;
+            set => SetAndRaise(UrlProperty, ref _url, value);
+        }
+
+        protected override void OnPointerPressed(PointerPressedEventArgs e)
+        {
+            base.OnPointerPressed(e);
+
+            NetUtils.OpenExternalLink(Url);
+        }
+
+    }
+}
